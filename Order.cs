15ffc31af0206a633a958251f0db0aec24712f62
Spider-has <romanh998@gmail.com>
2025-04-09 19:13:02 +0300

    Complete Order Manager task

---
+++ Order.cs
@@ -0,0 +1,39 @@
+﻿struct OrderData
+{
+    public string ProductName;
+    public int ProductCount;
+    public string ClientName;
+    public string DeliveryAddress;
+    public OrderData()
+    {
+        ProductName = string.Empty;
+        ProductCount = 0;
+        ClientName = string.Empty;
+        DeliveryAddress = string.Empty;
+    }
+}
+class Order
+{
+    public OrderData orderData = new();
+    public bool IsDataConfirmed { get; private set; }
+    private DateTime _orderConfirmationTime;
+    private static int _deliveryDayCount = 3;
+    public void SetOrderConfirmed()
+    {
+        IsDataConfirmed = true;
+        _orderConfirmationTime = DateTime.Now;
+    }
+    public void PrintOrderConfirmationMessage()
+    {
+        if ( IsDataConfirmed )
+        {
+            Console.WriteLine( $"{orderData.ClientName}! Ваш заказ {orderData.ProductName} в количестве {orderData.ProductCount} оформлен! Ожидайте доставку по адресу {orderData.DeliveryAddress} к {_orderConfirmationTime.AddDays( _deliveryDayCount )}" );
+        }
+        else
+        {
+            Console.WriteLine( "Похоже ваш заказ ещё не подтвержден, произошла какая-то ошибка, попробуйте пересоздать заказ" );
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine( "\nДобро пожаловать! Здесь вы можете создавать заказы!\n" );
+        OrdersCreationProcess();
+        Console.WriteLine( "\nСпасибо, что пользовались нашим приложением, до встречи!" );
+    }
+
+    static void OrdersCreationProcess()
+    {
+        bool isContinued = true;
+        while ( isContinued )
+        {
+            Order order = new();
+            ReadAllOrderData( ref order.orderData );
+            if ( CheckOrderConfirmation( order.orderData ) )
+            {
+                order.SetOrderConfirmed();
+                order.PrintOrderConfirmationMessage();
+            }
+            else
+            {
+                Console.WriteLine( "\nВ таком случае вы можете пересоздать заказ с измененными данными" );
+            }
+            isContinued = IsUserWantToRecreateOrder();
+        }
+    }
+    static string ReadNonEmptyString()
+    {
+        string input = string.Empty;
+        while ( string.IsNullOrEmpty( input ) )
+        {
+            input = Console.ReadLine();
+        }
+        return input;
+    }
+    static int ReadPositiveNumber()
+    {
+        int num = 0;
+        string numStr = string.Empty;
+        while ( !int.TryParse( numStr, out num ) || num <= 0 )
+        {
+            numStr = Console.ReadLine();
+        }
+        return num;
+    }
+
+    static bool ReadQuestionAnswer()
+    {
+        string answer = "";
+        while ( answer.ToLower() != "y" && answer.ToLower() != "n" )
+        {
+            answer = ReadNonEmptyString();
+        }
+        return answer.ToLower() == "y";
+    }
+
+    static void ReadAllOrderData( ref OrderData orderData )
+    {
+        Console.WriteLine( "\nВведите все параметры заказа.\n" );
+        Console.Write( "Введите название продукта(название не может быть пустым или состоять из пробелов): " );
+        orderData.ProductName = ReadNonEmptyString();
+        Console.Write( "Введите количество продукта(количество должно быть больше 0): " );
+        orderData.ProductCount = ReadPositiveNumber();
+        Console.Write( "Введите ваше имя(имя не может быть пустым или состоять из пробелов): " );
+        orderData.ClientName = ReadNonEmptyString();
+        Console.Write( "Введите адрес доставки(адрес не может быть пустым или состоять из пробелов): " );
+        orderData.DeliveryAddress = ReadNonEmptyString();
+    }
+
+    static bool CheckOrderConfirmation( OrderData orderData )
+    {
+        Console.WriteLine( $"\nВаше имя: {orderData.ClientName}, вы заказали {orderData.ProductCount} {orderData.ProductName} на адрес {orderData.DeliveryAddress}, всё верно? (Y / N) " );
+        return ReadQuestionAnswer();
+    }
+    static bool IsUserWantToRecreateOrder()
+    {
+        Console.WriteLine( "\nЖелаете ли вы создать новый заказ? (Y / N)" );
+        return ReadQuestionAnswer();
+    }
+}

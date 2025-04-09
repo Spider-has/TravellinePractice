9da1e6f34dd8b1707ee03de86c3a1299e858c6dc
Spider-has <romanh998@gmail.com>
2025-04-10 00:56:03 +0300

    complete Dictionary hometask#2

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,51 @@
+﻿namespace Dictionary
+{
+    internal class EngRusDictionary
+    {
+        private Dictionary<string, string> _dictionary;
+
+        public EngRusDictionary()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+        public void AddWordsCouple( string rus, string eng ) => _dictionary[ rus.ToLower() ] = eng.ToLower();
+        public bool GetEngByRusWord( string rus, out string translation )
+        {
+            if ( _dictionary.ContainsKey( rus ) )
+            {
+                translation = _dictionary[ rus ];
+                return true;
+            }
+            translation = string.Empty;
+            return false;
+        }
+        public bool GetRusByEngWord( string translation, out string rus )
+        {
+            if ( _dictionary.ContainsValue( translation ) )
+            {
+                rus = _dictionary.FirstOrDefault( x => x.Value == translation ).Key;
+                return true;
+            }
+            rus = string.Empty;
+            return false;
+        }
+
+        public void PrintAllWords()
+        {
+            foreach ( var pair in _dictionary )
+            {
+                Console.WriteLine( $"key: {pair.Key}  value: {pair.Value}" );
+            }
+        }
+
+        public List<string> DictionaryToStringsFileFormat()
+        {
+            List<string> strings = new();
+            foreach ( var pair in _dictionary )
+            {
+                strings.Add( $"{pair.Key}:{pair.Value}" );
+            }
+            return strings;
+        }
+    }
+}
---
+++ Dictionary/DictionaryFileManager.cs
@@ -0,0 +1,71 @@
+﻿namespace Dictionary
+{
+    internal class DictionaryFileManager
+    {
+        static string _fileName = "dictionary.txt";
+        static string separator = ":";
+        public static void InitOneLineFromFile( ref EngRusDictionary dictionary, string line )
+        {
+            bool err = false;
+            if ( line.Split( separator ).Length == 2 )
+            {
+                string rus = line.Split( separator )[ 0 ];
+                string eng = line.Split( separator )[ 1 ];
+                if ( LanguageChecker.IsWordRus( rus ) && LanguageChecker.IsWordEng( eng ) )
+                {
+                    dictionary.AddWordsCouple( rus, eng );
+                }
+                else err = true;
+            }
+            else err = true;
+            if ( err )
+            {
+                Console.WriteLine( $"Возникли ошибки при чтении строки {line} из файла" );
+            }
+        }
+        public static void FillDictionaryFromFile( ref EngRusDictionary dictionary, StreamReader readingFile )
+        {
+            string line;
+            while ( ( line = readingFile.ReadLine() ) != null )
+            {
+                InitOneLineFromFile( ref dictionary, line );
+            }
+        }
+
+        public static void FillFileFromDictionary( ref EngRusDictionary dictionary )
+        {
+            if ( CheckFile( _fileName ) )
+                using ( StreamWriter writer = new StreamWriter( _fileName, false ) )
+                {
+                    List<string> strings = dictionary.DictionaryToStringsFileFormat();
+                    foreach ( string s in strings )
+                    {
+                        writer.WriteLine( s );
+                    }
+                }
+        }
+
+        static public bool CheckFile( string path )
+        {
+            if ( File.Exists( path ) )
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine( $"не удалось открыть файл по пути {path}, файл не существует" );
+                return false;
+            }
+        }
+
+        public static void InitDictionary( ref EngRusDictionary dictionary )
+        {
+
+            if ( CheckFile( _fileName ) )
+                using ( StreamReader reader = new StreamReader( _fileName ) )
+                {
+                    FillDictionaryFromFile( ref dictionary, reader );
+                }
+        }
+    }
+}
